61c538f2b12f5fc02c8a7dc3b9c9759983bb2c1f
PavelZhukV <32051703+PavelZhukV@users.noreply.github.com>
2018-02-21 22:22:09 +0300

    Initial commit

---
+++ Zhuk.2018.2S.1/UnitTest1/unittest1.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Work1Library.h"
+#include <iostream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int array[] = { 1,5,16,7,20,22,30 } ;
+			int narray[] = { 0 };
+			int n = 6,k;
+			char a = 'E';
+
+			
+		}
+	
+	};
+}
